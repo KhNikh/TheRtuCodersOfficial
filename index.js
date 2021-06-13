123e4d11b7c2355ef6a7df4d2560194eced22163
Lokesh Singh <lksrajawat@gmail.com>
2021-06-13 10:40:23 +0530

    sunday

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+
+var posts = [{title:"First Post",body:"There are many variations of passages of Lorem Ipsum available, but the. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source."},{title:"Second Post",body:"There are many variations of passages of Lorem Ipsum available, but the. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source."},{title:"Third Post",body:"There are many variations of passages of Lorem Ipsum available, but the. Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source."}]
+
+
+
 app.get("/", (req, res) => {
   res.render("home");
 });
@@ -23,8 +28,20 @@ app.get("/resources", (req, res) => {
   res.render("resources");
 });
 
-app.get("/discuss", (req, res) => {
-  res.render("discuss");
+app.get("/discuss",function(req,res){
+  res.render("discuss",{posts})
+});
+
+app.get("/discuss/:newpage", function (req, res) {
+  posts.forEach(function (post) {
+    if (post.title == req.params.newpage) {
+      var title = post.title;
+      var body = post.body;
+      res.render("home",{title,body});
+      console.log("Match found");
+      return;
+    }
+  });
 });
 
 
